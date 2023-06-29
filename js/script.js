c068f33f1bf13d3337116a570d8780d4eda35001
guzzunc <77429704+guzzunc@users.noreply.github.com>
2023-06-29 13:19:13 +0300

    new-contact

--- js/script.js
+++ js/script.js
@@ -49,3 +49,43 @@ btnDarkMode.onclick = function () {
     } 
 }
 
+
+// Scroll Up Button
+let mybutton = document.getElementById("TopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// Form Excel
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxync-ay4fwor_5y_736OcQIRyfHABBCSl51yD7mbTo7ce8B6fQbbnxayyTFgFzoY92/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("form__msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },4000)
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
